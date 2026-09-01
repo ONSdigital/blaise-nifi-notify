1a7a8bc425bf8b4241f32780f9141b3f0a3ee49d
lambeb <141648830+lambeb@users.noreply.github.com>
2026-09-01 14:55:57 +0100

    changed test file for new addition

--- tests/test_models/test_message.py
+++ tests/test_models/test_message.py
@@ -38,6 +38,7 @@ def test_file_type(file, file_name, file_type):
         ("dd_lmc2102_bk1.zip", "LMC"),
         ("dd_lmb21021_bk2.zip", "LMB"),
         ("dd_frs2411a.zip", "FRS"),
+        ("dd_scs2411a.zip", "SCS"),
     ],
 )
 def test_file_survey_tla(file, file_name, expected):
@@ -53,6 +54,7 @@ def test_file_survey_tla(file, file_name, expected):
         ("dd_lms2102_bk1.zip", "LMS2102_BK1"),
         ("dd_lmc2102_bk1.zip", "LMC2102_BK1"),
         ("dd_frs2411a.zip", "FRS2411A"),
+        ("dd_scs2411a.zip", "SCS2411A"),
     ],
 )
 def test_file_instrument_name(file, file_name, expected):
@@ -74,6 +76,7 @@ def test_file_instrument_name(file, file_name, expected):
         ("LMNOP", True),
         ("LBS", False),
         ("FRS", False),
+        ("SCS", False),
     ],
 )
 def test_file_is_lms(file, survey_tla, expected):
@@ -95,6 +98,7 @@ def test_file_is_lms(file, survey_tla, expected):
         ("LMNOP", False),
         ("LBS", False),
         ("FRS", True),
+        ("SCS", False),
     ],
 )
 def test_file_is_frs(file, survey_tla, expected):
@@ -116,6 +120,7 @@ def test_file_from_event(dd_event):
         ("opn2101A", "OPN"),
         ("lms2102_bk1", "LMS"),
         ("frs2102a", "FRS"),
+        ("scs2102a", "SCS"),
     ],
 )
 def test_create_message_for_management_information(
@@ -163,6 +168,22 @@ def test_create_message_for_data_delivery_frs(dd_event, config):
     assert actual_message.iterationL2 == "survey_data"
     assert actual_message.iterationL3 == "bl5-test"
     assert actual_message.iterationL4 == "FRS2411A"
+
+
+def test_create_message_for_data_delivery_scs(dd_event, config):
+    dd_event = dd_event("scs2411a")
+    actual_message = create_message(dd_event, config)
+
+    assert (
+        actual_message.description
+        == "Data Delivery files for SCS uploaded to GCP bucket from Blaise5"
+    )
+
+    assert actual_message.dataset == "blaise_dde_scs"
+    assert actual_message.iterationL1 == "Crime Statistics Data"
+    assert actual_message.iterationL2 == "BL5-test"
+    assert actual_message.iterationL3 == "SCS"
+    assert actual_message.iterationL4 == "SCS2411A"
 
 
 @pytest.mark.parametrize(
